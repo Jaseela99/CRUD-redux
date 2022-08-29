058d4aff06d12213f15c7ea83f0e0dd0d785ea99
Jaseela99 <jaseelachittur@gmail.com>
2022-08-29 12:50:47 +0530

    added userList

---
+++ crud/src/App.js
@@ -0,0 +1,12 @@
+
+import UserList from './features/users/UserList';
+function App() {
+  return (
+    <div className="container mx-auto px-2 max-w-5xl pt-10 md:pt-32 ">
+<h1 className='text-center font-bold text-2xl text-gray-700'>CRUD</h1>
+<UserList/>
+    </div>
+  );
+}
+
+export default App;
---
+++ crud/src/features/users/UserList.js
@@ -0,0 +1,34 @@
+import React from 'react'
+const users =[
+{
+id:1,
+name:"jaseela",
+email:"jaseela@gmail.com"
+},
+{
+id:2,
+name:"jaseena",
+email:"jaseena@gmail.com"
+}
+]
+const renderCard=()=>
+    users.map((user)=>(
+        <div className='bg-gray-300 p-5 flex items-center justify-between'>
+            <div>
+                <h3 className='font-bold text-lg text-gray-700'>{user.name}</h3>
+                <span className='font-normal text-gray-600'>{user.email}</span>
+            </div>
+            </div>
+    ))
+
+const UserList = () => {
+  return (
+    <div>
+        <div className='grid gap-5 md:grid-cols-2'>
+        {users.length ? renderCard():<p className='text-center col-span-2 text-gray-700 font-semibold'>No User Found</p>}
+        </div>
+    </div>
+  )
+}
+
+export default UserList
