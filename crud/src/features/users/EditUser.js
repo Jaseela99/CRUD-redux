064456198b21e41a21a0dcb7b27727a7b30444dd
Jaseela99 <jaseelachittur@gmail.com>
2022-09-05 17:28:34 +0530

    edituser done

--- crud/src/features/users/EditUser.js
+++ crud/src/features/users/EditUser.js
@@ -1,16 +1,27 @@
 import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useSelector,useDispatch } from 'react-redux'
+import { useNavigate,useParams} from 'react-router-dom'
 import Button from '../../Components/Button'
 import TextField from '../../Components/TextField'
-
+import { editUser } from './userSlice'
 const EditUser = () => {
+  const dispatch = useDispatch()
+  const users=useSelector(store=>store.users)
+  const params=useParams()
+  const existingUser=users.filter(user=>user.id===params.id)
+  const {name,email}=existingUser[0]
     const navigate=useNavigate()
     const [values,setValues]=React.useState({
-        name:"",
-        email:""
+        name:name,
+        email:email
     })
     const handleEditUser=()=>{
        setValues({name:"",email:""})
+       dispatch(editUser({
+        id:params.id,
+         name:values.name,
+         email:values.email
+       }))
        navigate("/")
     }
   return (
